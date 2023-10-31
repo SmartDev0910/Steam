393f6e36a4b170cf0efd1772623a3023763bdc4c
SmartDev0910 <SuperNames0910@gmail.com>
2023-10-31 15:29:46 +0800

    add application type, update config

--- Backend/models/member.js
+++ Backend/models/member.js
@@ -1,31 +1,60 @@
 const mongoose = require("mongoose");
 
 const memberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
   email: {
     type: String,
     trim: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+  },
+  passwordLastChanged: {
+    type : String,
   },
   steam64: {
     type: String,
     trim: true,
   },
-  discordId: {
+  discordID: {
     type: String,
     trim: true,
-  },
-  isWhiteListed: {
-    type: Boolean,
-    default: false,
   },
   isBanned: {
     type: Boolean,
     default: false,
   },
-  ipAddress: {
+  mfaEnabled: {
+    type: Boolean,
+    default: false
+  },
+  mfaPhoneNumber: {
     type: String,
     trim: true,
   },
-  password: {
+  role: {
+    type: String,
+    trim: true,
+  },
+  applications: [{
+    applicationTypeId: {
+      type: String,
+      trim: true,
+    },
+    audio: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'approved', 'rejected'],
+      default: 'pending',
+    }
+  }],
+  ip: {
     type: String,
     trim: true,
   },
